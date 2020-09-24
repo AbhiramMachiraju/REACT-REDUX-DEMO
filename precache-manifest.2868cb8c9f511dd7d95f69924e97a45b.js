c9b7e73956fe244729f9bfadcfa6c6a4c7809b5d
ABHIRAM <abhiramayasaki25@gmail.com>
2020-09-24 10:22:41 +0530

    Updates

---
+++ precache-manifest.2868cb8c9f511dd7d95f69924e97a45b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745a088d8c120055004a176cd560a080",
+    "url": "/AbhiramMachiraju/REACT-REDUX-FIRSTDEMO.git/index.html"
+  },
+  {
+    "revision": "53b75aa472f4798418a5",
+    "url": "/AbhiramMachiraju/REACT-REDUX-FIRSTDEMO.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fd74c8a90199d446e13b",
+    "url": "/AbhiramMachiraju/REACT-REDUX-FIRSTDEMO.git/static/js/2.e4aab4f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AbhiramMachiraju/REACT-REDUX-FIRSTDEMO.git/static/js/2.e4aab4f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53b75aa472f4798418a5",
+    "url": "/AbhiramMachiraju/REACT-REDUX-FIRSTDEMO.git/static/js/main.81754208.chunk.js"
+  },
+  {
+    "revision": "6cdcd39ca3627bf9fbff",
+    "url": "/AbhiramMachiraju/REACT-REDUX-FIRSTDEMO.git/static/js/runtime-main.1325615e.js"
+  }
+]);
